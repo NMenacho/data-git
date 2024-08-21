9d7e849a5e26b6239e30316c2115a51896565a5a
Natalia Menacho Rojas <natalia.mr16@outlook.com>
2024-08-21 19:32:09 +0200

    Fixed style issues, should get a 'Good style now'

--- today.py
+++ today.py
@@ -2,7 +2,13 @@
 This is the module docstring
 '''
 def my_name_is():
+    '''
+    This is the function docstring
+    '''
     return "nmenacho"
 
 def my_buddy_is():
+    '''
+    This is the function docstring
+    '''
     return "kann"
